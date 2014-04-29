14c4d388270c97251ca09423a021d2a43fa474f0
Amrita-S-Ubuntu <amrita-s-ubuntu@amritasubuntu-OptiPlex-3010.(none)>
2014-04-29 18:09:51 +0530

    controller

--- app/controllers/friendships_controller.rb
+++ app/controllers/friendships_controller.rb
@@ -3,4 +3,15 @@ class FriendshipsController < ApplicationController
 	def get_friends
 		@friendship = User.first.friends
 	end
+
+	
+	def add_friend
+       @friend = User.first.friendships.build(friend_id:params[:friend_id])
+        if @friend.save!
+        	@result= "added"
+        else	
+        	@result= "we both r not friend of own"
+        end 	 
+   end
+
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,20 @@
 class UsersController < ApplicationController
+	
 	def registration
-		
-	end
+       @newuser =User.new(:first_name=>params[:first_name], :last_name=>params[:last_name], :username=>params[:username], :password=>params[:password], :email=>params[:email], phone_number:params[:phone_number], address:params[:address])
+       if @newuser.save!
+       	 @result="User created successfully"
+       else
+         @result="Error" 
+       end
+    end
 
 	def login
-
+		@authenticate_user=User.find_by_username_and_password(params[:username],params[:password])
+		 if @authenticate_user.present?
+		 	@result="login successfully"
+         else
+            @result="login unsuccessfully"
+         end 
 	end
 end
