b99e5c379325d70cb2012025bce98c6989a555af
Biju Vidyadharan <biju.vidyadharan@diaspark.com>
2014-05-06 12:52:11 +0530

    mongo implemented

--- Gemfile
+++ Gemfile
@@ -26,6 +26,9 @@ gem 'sdoc', '~> 0.4.0',          group: :doc
 #Google Cloud Messaging for Android on Rails
 gem 'simple_gcm'
 
+gem 'mongoid', git: 'https://github.com/mongoid/mongoid.git'
+gem 'bson_ext'
+
 # Spring speeds up development by keeping your application running in the background. Read more: https://github.com/rails/spring
 gem 'spring',        group: :development
 
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,7 @@ class User < ActiveRecord::Base
 	has_many :inverse_friendships, :class_name => "Friendship", :foreign_key => "friend_id"
 	has_many :inverse_friends, :through => :inverse_friendships, :source => :user
 	has_one :api_key, dependent: :destroy
+	#has_many :chats
 	# implemented scope for the understanding purpose.
 	scope :pass, -> { where(password: 1234567) }
 	scope :other_friends_only, -> { joins(:friends)}
@@ -16,6 +17,19 @@ class User < ActiveRecord::Base
         	return false
         end
   	end
+
+  	def chats
+		@chats ||= (Chat.where(:user_id => self.id))
+	end
+
+	def create_chats message, friend_id
+		if message.present? && friend_id.present?
+			@chat = Chat.create(user_id: self.id, message: message, friend_id: friend_id, created_at: Time.now)
+		else
+			raise "Should have message/friend_id"
+		end
+	end
+
 end
 
 
